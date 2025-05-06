a6655a7e0341b9d65b17cbec33866e82f252c080
Alex Beltran <alex.beltran@open-eb.co>
2025-05-06 08:24:20 -0500

    Modificaciones a operaciones.ts

--- operaciones.ts
+++ operaciones.ts
@@ -1,3 +1,11 @@
 function suma(a: number, b: number): number {
     return a+b;
 }
+
+function dividir(a: number, b: number) {
+    try {
+        return a+b;
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+}
