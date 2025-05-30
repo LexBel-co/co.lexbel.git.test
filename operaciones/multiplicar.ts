8263cb5ad38116b98d72a001ee5fc20a0671ba94
crixus12cr <cristian2020til@gmail.com>
2025-05-30 07:32:25 -0500

    feat: se agrega el metodo doble que calcula el doble de un numero

--- operaciones/multiplicar.ts
+++ operaciones/multiplicar.ts
@@ -1,9 +1,9 @@
-/**
- * Calcula el elevar de dos números.
- * @param a - El primer número
- * @param b - El segundo número
- * @returns El resultado de multiplicar a * b
- */
+
 function elevar(a: number, b: number): number {
   return a * b;
+}
+
+
+function doble(a: number): number {
+  return a * 2;
 }
