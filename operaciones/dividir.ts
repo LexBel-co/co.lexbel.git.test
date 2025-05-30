17ac996b8e82f13217b8609600cdcd97d8b35a9c
Juan Trujillo <juan.trujillo@open-eb.co>
2025-05-30 07:42:02 -0500

    docs: agrega la documentación a las funciones operacionDividir y operacionSumar

--- operaciones/dividir.ts
+++ operaciones/dividir.ts
@@ -1,4 +1,10 @@
-
+/**
+ * Permite dividir dos números.
+ *
+ * @param {number} a Número a dividir a.
+ * @param {number} b Número a dividir b.
+ * @return {*} 
+ */
 function operacionDividir(a: number, b: number) {
     try {
         return a+b;
@@ -7,6 +13,14 @@ function operacionDividir(a: number, b: number) {
     }
 }
 
+/**
+ * Permite sumar tres números.
+ *
+ * @param {number} a Nmúmero a sumar a.
+ * @param {number} b Número a sumar b.
+ * @param {number} c Número a sumar c.
+ * @return {*}  {number}
+ */
 function operacionSumar(a: number, b: number, c: number): number {
     return a+b+c;
 }
