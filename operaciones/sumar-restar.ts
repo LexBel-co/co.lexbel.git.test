5e14b1d00dd5ac0fe86049c0fa9bb67902439679
Juan Hernández <08.hernandez.jj@gmail.com>
2025-05-30 08:04:58 -0500

    feat(sumar-restar): Se agrega el método para obtener el valor absoluto de un número en el archivo sumar-restar.ts

--- operaciones/sumar-restar.ts
+++ operaciones/sumar-restar.ts
@@ -5,3 +5,7 @@ function opSumar(a: number, b: number, c: number): number {
 function opLess(a: number, b: number): number {
     return a-b;
 }
+
+function opAbsolute(a: number): number {
+    return a < 0 ? (a * -1) : (1 * a);
+}
