4159a454018b92758e755a4dd85bf1736a6a708a
Juan Hernández <08.hernandez.jj@gmail.com>
2025-05-30 08:05:27 -0500

    docs(sumar-restar): Se agregó la documentación del método de la operación sumar en el archivo sumar-restar.ts

--- operaciones/sumar-restar.ts
+++ operaciones/sumar-restar.ts
@@ -1,3 +1,12 @@
+/**
+ * Operación para realizar la suma de 3 números.
+ * 
+ * @param {number} a Numero A
+ * @param {number} b Numero B
+ * @param {number} c Numero C
+ * 
+ * @return number
+ */
 function opSumar(a: number, b: number, c: number): number {
     return a+b+c;
 }
