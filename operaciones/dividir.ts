6188d6d0769e067ef17c43f68ff54769b4a84516
jjtrujillo1199 <85378318+jjtrujillo1199@users.noreply.github.com>
2025-06-10 13:30:32 -0500

    Agrega y modifica funcionalidades en el archivo dividir (#15)
    * refactor: renombra la función dividir por operacionDividir
    
    * feat: agrega la función operacionSumar
    
    * docs: agrega la documentación a las funciones operacionDividir y operacionSumar
    
    * feat: Crea el método para calcular el doble de un número, modifica la documentación en las funciones y cambia la lógica en el método operacionSumar
    
    ---------
    
    Co-authored-by: Juan Trujillo <juan.trujillo@open-eb.co>

--- operaciones/dividir.ts
+++ operaciones/dividir.ts
@@ -1,8 +1,35 @@
-
-function dividir(a: number, b: number) {
+/**
+ * Modificación del comentario, permite dividir dos números.
+ *
+ * @param {number} a Número a dividir a.
+ * @param {number} b Número a dividir b.
+ * @return {*} 
+ */
+function operacionDividir(a: number, b: number) {
     try {
         return a+b;
     } catch (error) {
         console.error(`Error: ${error}`);
     }
+}
+
+/**
+ * Permite sumar dos números.
+ *
+ * @param {number} a Nmúmero a sumar a.
+ * @param {number} b Número a sumar b.
+ * @return {*}  {number}
+ */
+function operacionSumar(a: number, b: number): number {
+    return a+b;
+}
+
+/**
+ * Calcula el doble de un número.
+ *
+ * @param {number} a Numero a multiplicar por 2.
+ * @return {*}  {number}
+ */
+function operacionDoble(a: number): number {
+    return a * 2;
 }
